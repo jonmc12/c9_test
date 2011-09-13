97e011fc23312421c2c5dc5291eec62422a97b08
Jonathan McCoy <jonathan@perceptionlabs.com>
2011-09-12 17:53:41 -0700

    Initial commit - new rails 3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+C9Test::Application.config.secret_token = '96ce1c149780631f2308d60e6111d90fa57bd21d83a3d6f78e86d568f9847b0eba82dafbd2f7c6091e01a6104b2256fb878c8ed59edb6ebcaa8636e53867219f'
